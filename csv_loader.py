ae09fad2a9e9aa2bd6f26538cb901592d9812567
Izzy Lancaster <avrisaac555@gmail.com>
2023-07-14 17:07:08 -0700

    Added output options, refactored loading

--- csv_loader.py
+++ csv_loader.py
@@ -3,10 +3,21 @@ import pandas
 
 from argparse import ArgumentParser, Namespace
 from collections.abc import Sequence
-from csv import DictReader
+from enum import Enum
 from typing import Any, Callable, TypeVar
 from pandas import DataFrame
 from pandas.api.extensions import no_default
+
+def read_csv_dataframe(file_path: str, 
+             parse_dates: bool | list | dict = True,
+             names: list | None = None,
+             skipinitialspace = True,
+             fillna_value = 0,
+             **read_csv_args) -> DataFrame:
+    """Wrapper around pandas read_csv to provide better defaults"""
+    dataframe: DataFrame = pandas.read_csv(file_path, parse_dates=parse_dates, names=names, skipinitialspace=skipinitialspace, **read_csv_args)
+    dataframe.fillna(fillna_value, inplace=True)
+    return dataframe
 
 OpFactory = Callable[..., Callable[[DataFrame], None]]
 
@@ -44,10 +55,21 @@ class _OperationAction(argparse.Action):
 
         getattr(namespace, self.dest).append(operation)
 
+class _OutputActions(Enum):
+    PRINT = 1
+    LONG_PRINT = 2
+    CSV = 3
+
 arg_parser = argparse.ArgumentParser(description="Imports a CSV file into the transaction database.")
 arg_parser.add_argument("csv", help="Path to the CSV file to import.")
-arg_parser.add_argument("--db", default="finances.db", help="Path to the database to insert into.")
 arg_parser.add_argument("--colname", "--colnames", action="extend", nargs="*", help="The names of the columns. If none are provided, the first row will be used.")
+
+_output_actions_grp = arg_parser.add_argument_group("Output Actions")
+_output_actions_grp.add_argument("--print",      action="store_const", dest="output", const=_OutputActions.PRINT)
+_output_actions_grp.add_argument("--long-print", action="store_const", dest="output", const=_OutputActions.LONG_PRINT, help="Print out the entirety of the data.")
+_output_actions_grp.add_argument("--csv-print",  action="store_const", dest="output", const=_OutputActions.CSV, help="Print out the data as a CSV.")
+_output_actions_grp.add_argument("--db", nargs=1, metavar="PATH", help="Path to the database to write the data to.")
+arg_parser.set_defaults(output=_OutputActions.PRINT)
 
 _col_ops_grp = arg_parser.add_argument_group("Column Operations")
 _col_ops_grp.add_argument("--rename",  action=_OperationAction, dest="opers", op_func=rename_operation, nargs=2, metavar=("OLD NAME", "NEW NAME"), help="Renames a column")
@@ -57,16 +79,26 @@ _col_ops_grp.add_argument("--drop", action=_OperationAction, dest="opers", op_fu
 def _main():
     args = arg_parser.parse_args()
 
-    with open(args.csv) as csv_file_handle:
-        csv_data = pandas.read_csv(csv_file_handle, names=args.colname or no_default, na_values=[0])
-        csv_data.fillna(0, inplace=True)
+    csv_data = read_csv_dataframe(args.csv, names=args.colname or None)
 
-        # Execute operations on the data
-        if args.opers is not None:
-            for op in args.opers:
-                op(csv_data)
+    # Execute operations on the data
+    if args.opers is not None:
+        for op in args.opers:
+            op(csv_data)
 
-        print(csv_data)
+    # Output
+    match args.output:
+        case _OutputActions.PRINT:
+            print(csv_data)
+            return
+        case _OutputActions.LONG_PRINT:
+            print(csv_data.to_string())
+            return
+        case _OutputActions.CSV:
+            print(csv_data.to_csv())
+            return
+        
+        
 
 if __name__ == "__main__":
     _main()

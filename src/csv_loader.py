494e7711c18eec1a47c7feec1eb631db7d544d0b
Izzy Lancaster <avrisaac555@gmail.com>
2023-07-15 20:57:32 -0700

    Script now loads tags into db

--- src/csv_loader.py
+++ src/csv_loader.py
@@ -1,12 +1,15 @@
+import apsw.ext
 import argparse
 import logging
 import pandas
-import sqlite3
 
+from apsw import Connection
 from argparse import ArgumentParser, Namespace
 from collections.abc import Sequence
 from enum import Enum
-from typing import Any, Callable, TypeVar
+from sql import upsert_many_transactions, upsert_many_tags, insert_transaction_tag_relations
+from tagging import tag_description
+from typing import Any, Callable
 from pandas import DataFrame
 from pandas.api.extensions import no_default
 
@@ -27,6 +30,13 @@ def read_csv_dataframe(file_path: str,
     LOGGER.info("Read %i rows from csv", len(dataframe))
 
     return dataframe
+
+def tag_dataframe(data: DataFrame):
+    data["Tags"] = data["Description"].apply(tag_description)
+
+def collect_tags(data: DataFrame, tag_column: str = "Tags") -> set[str]:
+    """Collects tags from a DataFrame"""
+    return set(tag for tag_seq in data[tag_column] for tag in tag_seq)
 
 OpFactory = Callable[..., Callable[[DataFrame], None]]
 
@@ -50,29 +60,6 @@ def insert_operation(name, value):
     def op(data: DataFrame):
         data[name] = value
     return op
-
-def open_sqldb(path: str, readonly: bool = False):
-    """Opens a connection to the sqlite db on path."""
-    if path is None:
-        raise ValueError("path cannot be None")
-
-    mode = "ro" if readonly else "rw"
-    sqlite_uri = f"file:{path}?mode={mode}"
-    return sqlite3.connect(sqlite_uri, uri=True)
-
-def create_transactions_insert_query(
-        columns: list[str] = ["Description", "PostDate", "Amount", "Source"],
-        parameter_names: list[str] = ["Description", "Date", "Amount", "Source"],
-        ignore_duplicates = True):
-    """Creates an SQL insert query to be used with insertmany"""
-
-    columns = ", ".join(columns)
-    parameters = ", ".join(":" + p for p in parameter_names)
-    or_ignore = " OR IGNORE" if ignore_duplicates else ""
-    query = f"INSERT{or_ignore} INTO transactions({columns}) VALUES({parameters})"
-
-    LOGGER.debug("Created SQL insert query `%s`", query)
-    return query
 
 class _OperationAction(argparse.Action):
     def __init__(self, option_strings: Sequence[str], dest: str, op_func: OpFactory, **kwargs) -> None:
@@ -100,7 +87,6 @@ class _OutputActions(Enum):
 arg_parser = argparse.ArgumentParser(description="Imports a CSV file into the transaction database.")
 arg_parser.add_argument("csv", help="Path to the CSV file to import.")
 arg_parser.add_argument("--colname", "--colnames", action="extend", nargs="*", help="The names of the columns. If none are provided, the first row will be used.")
-arg_parser.add_argument("-d", "--dry-run", action="store_true", help="Output the result but don't actually modify any files on disk.")
 arg_parser.add_argument("-v", "--verbose", action="store_true")
 
 _output_actions_grp = arg_parser.add_argument_group("Output Actions")
@@ -117,15 +103,19 @@ _col_ops_grp.add_argument("--drop",    action=_OperationAction, dest="opers", op
 _col_ops_grp.add_argument("--insert",  action=_OperationAction, dest="opers", op_func=insert_operation, nargs=2, metavar=("NAME", "VALUE"))
 
 def _main():
-    LOGGER.addHandler(logging.StreamHandler())
+    logging.root.addHandler(logging.StreamHandler())
     args = arg_parser.parse_args()
 
     if args.verbose:
-        LOGGER.setLevel(logging.DEBUG)
+        apsw.ext.log_sqlite(level = logging.DEBUG)
+        logging.root.setLevel(logging.DEBUG)
+    else:
+        apsw.ext.log_sqlite()
 
     LOGGER.debug("Args parsed as %s", args)
 
     csv_data = read_csv_dataframe(args.csv, names=args.colname or None)
+    tag_dataframe(csv_data)
 
     # Execute operations on the data
     if args.opers is not None:
@@ -147,18 +137,16 @@ def _main():
     else:
         # args.db is set
         try:
-            db_conn = open_sqldb(args.db, readonly=args.dry_run)
-            with db_conn:
-                sql = create_transactions_insert_query()
-                records = csv_data.to_dict(orient="records")
+            records = csv_data.to_dict(orient="records")
+            db_connection = Connection(args.db)
+            with db_connection:
+                transaction_ids = upsert_many_transactions(db_connection, records)
+                tags = collect_tags(csv_data)
+                tag_ids = upsert_many_tags(db_connection, tags)
+                insert_transaction_tag_relations(db_connection, transaction_ids, records, tag_ids)
 
-                if args.dry_run:
-                    print(f"The operation would insert {len(records)} rows")
-                
-                cursor = db_conn.executemany(sql, records)
-                print(f"Inserted {cursor.rowcount} new rows")
         finally:
-            db_conn.close()
+            db_connection.close()
 
 
 if __name__ == "__main__":
--- src/sql.py
+++ src/sql.py
@@ -1,1 +1,53 @@
-import aspw
+import apsw
+import apsw.ext
+import logging
+
+from typing import Sequence
+
+LOGGER = logging.getLogger("sql.csv_loader")
+
+def upsert_many_transactions(conn: apsw.Connection, records: Sequence[dict]) -> list[int]:
+    """
+    Inserts all transactions into the db, ignoring if they already exist.
+    Returns a list of the upserted transaction ids in the db.
+    """
+    
+    sql = """
+        INSERT INTO transactions(Description, PostDate, Amount, Source)
+        VALUES (:Description, :Date, :Amount, :Source)
+        ON CONFLICT(Description, PostDate, Amount, Source) DO NOTHING
+        RETURNING id
+    """
+
+    cursor = conn.cursor()
+    cursor.executemany(sql, records)
+    return [columns[0] for columns in cursor]
+
+def upsert_many_tags(conn: apsw.Connection, tags: Sequence[str]) -> dict[str, int]:
+    """
+    Inserts all tags into the db, ignoring if they already exist.
+    Assumes iteration remains consistent if the sequence isn't modified.
+    Returns a dict mapping each tag to it's id in the db. This dict may be smaller than the sequence passed in due to duplicates. 
+    """
+
+    sql = """
+        INSERT INTO tags(name)
+        VALUES (?)
+        ON CONFLICT(name) DO NOTHING
+        RETURNING id
+    """
+
+    cursor = conn.cursor()
+    cursor.executemany(sql, ((t,) for t in tags))    
+    return {t: columns[0] for t, columns in zip(tags, cursor)}
+
+def insert_transaction_tag_relations(conn: apsw.Connection, transaction_ids: Sequence[int], transactions: list[dict], tag_ids: dict[str, int]):    
+    transaction_tag_relation = (
+        (transaction_id, tag_ids[tag]) for transaction_id, record in zip(transaction_ids, transactions) 
+                                       for tag in record["Tags"])
+    
+    sql = """INSERT INTO transaction_tags(transaction_id, tag_id) VALUES (?, ?)"""
+
+    cursor = conn.cursor()
+    cursor.executemany(sql, transaction_tag_relation)
+    return cursor
